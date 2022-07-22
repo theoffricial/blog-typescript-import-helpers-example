586d188bce82e1600d4d9476fdccd319e3916a72
unicop <its.op.the.unicorn@gmail.com>
2022-07-22 17:01:06 +0300

    add dist files on purpose

---
+++ dist/with-import-helpers-out-tsc/main-module.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+var tslib_1 = require("tslib");
+var math_main_module_1 = require("./math/math-main-module");
+console.log('1 + 2 =', (0, math_main_module_1.add)(1, 2));
+// To show how TypeScript handles with "async-await"
+(function bla() {
+    return tslib_1.__awaiter(this, void 0, void 0, function () {
+        return tslib_1.__generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, Promise.resolve(42)];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+});
+// To show how TypeScript handles "export star"
+tslib_1.__exportStar(require("./math/math-main-module"), exports);
---
+++ dist/with-import-helpers-out-tsc/math/add.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.add = void 0;
+function add(num1, num2) {
+    return num1 + num2;
+}
+exports.add = add;
